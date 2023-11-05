02fe89650aeb7bc331d37d405cb3174395178fbf
roman_dector <roman_dector@vk.com>
2023-11-05 22:35:25 +0300

    chore: set up eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,39 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'eslint-plugin-react',
+    'eslint-plugin-import',
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+
+    'no-console': 'warn',
+    curly: 'error',
+    'react/jsx-no-useless-fragment': [
+      'error',
+      { allowExpressions: true },
+    ],
+    'no-unused-expressions': 'error',
+    'import/prefer-default-export': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+      },
     ],
   },
 }
