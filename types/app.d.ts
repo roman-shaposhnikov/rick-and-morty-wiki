12613500690f6aaffc9c1cf291dc6952cef42f99
roman_dector <roman_dector@vk.com>
2023-11-05 21:41:55 +0300

    chore: create withStore hoc; update .eslintrc

--- types/app.d.ts
+++ types/app.d.ts
@@ -1,6 +1,7 @@
+type Store = import('app/providers/store.tsx').Store
 type RootState = import('app/providers/store.tsx').RootState
 type AppDispatch = import('app/providers/store.tsx').AppDispatch
 
 interface Window {
-  __store__: unknown
+  __store__: Store
 }
