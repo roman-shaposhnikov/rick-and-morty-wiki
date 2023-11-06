c694aadaaaeebed07d1c20ce54f7ba79ae86a253
roman_dector <roman_dector@vk.com>
2023-11-07 01:19:30 +0300

    chore: add base to vite config

--- src/pages/root/ui/root.tsx
+++ src/pages/root/ui/root.tsx
@@ -1,3 +1,10 @@
+import { Link } from 'react-router-dom'
+
 export function Root() {
-  return <h1>Root page</h1>
+  return (
+    <>
+      <h1>Root page</h1>
+      <Link to={'signup'}> laskdfj</Link>
+    </>
+  )
 }
