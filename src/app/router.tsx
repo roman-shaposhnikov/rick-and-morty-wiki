85d26e11a033586cc3ba1302672f49a369c7e271
roman_dector <roman_dector@vk.com>
2023-11-06 23:35:47 +0300

    chore: format under new eslint rules

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,6 +1,5 @@
-import { createBrowserRouter } from 'react-router-dom'
-
 import { Root } from 'pages/root'
+import { createBrowserRouter } from 'react-router-dom'
 
 export const router = createBrowserRouter([
   {
