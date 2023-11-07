47595dbf91e5c763eb0725b66650d6112a050cb5
roman_dector <roman_dector@vk.com>
2023-11-07 20:36:24 +0300

    fix: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+export default (params: { mode: string }) => {
+  const env = loadEnv(params.mode, process.cwd(), '')
 
   return defineConfig({
     plugins: [react(), tsconfigPaths()],
