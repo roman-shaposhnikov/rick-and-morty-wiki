4162f77de032e86882c3f1bea5940398c9237256
roman_dector <roman_dector@vk.com>
2023-11-10 02:15:00 +0300

    feat: add localStorage auth helpers

---
+++ src/shared/api/auth/local-storage/lib.ts
@@ -0,0 +1,40 @@
+import { Credentials, User } from '../interfaces'
+
+const REGISTERED_USERS_LS_KEY = 'registered-users'
+const CURRENT_USER_ID_LS_KEY = 'current-user-id'
+
+export function shouldSignout() {
+  return !!localStorage.getItem(CURRENT_USER_ID_LS_KEY)
+}
+
+export function getUsers(): User[] {
+  const stringUsers = localStorage.getItem(REGISTERED_USERS_LS_KEY)
+
+  if (stringUsers === null) {
+    return []
+  }
+
+  return JSON.parse(stringUsers)
+}
+
+export function addUser(user: User) {
+  const users = getUsers()
+
+  users.push(user)
+
+  localStorage.setItem(REGISTERED_USERS_LS_KEY, JSON.stringify(users))
+}
+
+export function findUserByCreds(creds: Credentials) {
+  const users = getUsers()
+
+  return users.find(
+    u =>
+      u.creds.username === creds.username &&
+      u.creds.password === creds.password
+  )
+}
+
+export function setCurrentUserId(id: string) {
+  localStorage.setItem(CURRENT_USER_ID_LS_KEY, id)
+}
