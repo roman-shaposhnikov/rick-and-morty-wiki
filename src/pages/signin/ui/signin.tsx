3bdd02703cb5d9b16016e6113c72ccdf5f21dbac
roman_dector <roman_dector@vk.com>
2023-11-10 20:05:49 +0300

    feat: put SignInForm

--- src/pages/signin/ui/signin.tsx
+++ src/pages/signin/ui/signin.tsx
@@ -1,3 +1,14 @@
+import { SignInForm } from 'features/signin'
+import { Link } from 'react-router-dom'
+
 export function SignIn() {
-  return <h1>SignIn page</h1>
+  return (
+    <main className='page authPage'>
+      <h1>Sign In</h1>
+      <SignInForm />
+      <Link to={'/signup'}>
+        Don't you have an account? Just Sign Up!
+      </Link>
+    </main>
+  )
 }
