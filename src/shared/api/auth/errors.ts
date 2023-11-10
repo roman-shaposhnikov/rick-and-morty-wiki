449f240e793b5b6d78ef108768ae3934beed909e
roman_dector <roman_dector@vk.com>
2023-11-10 18:36:55 +0300

    ref: put errors names in a separate constants

--- src/shared/api/auth/errors.ts
+++ src/shared/api/auth/errors.ts
@@ -1,22 +1,25 @@
+export const USER_NOT_REGISTERED = 'UserNotRegistered'
 export class UserNotRegisteredError extends Error {
   constructor() {
     super('The user must be registered before the sign in process')
-    this.name = 'UserNotRegistered'
+    this.name = USER_NOT_REGISTERED
   }
 }
 
+export const USER_ALREADY_EXISTS = 'UserAlreadyExists'
 export class UserAlreadyExistsError extends Error {
   constructor() {
     super('The user is already registered, just sign in')
-    this.name = 'UserAlreadyExists'
+    this.name = USER_ALREADY_EXISTS
   }
 }
 
+export const USER_SHOULD_SIGNOUT = 'UserShouldSignout'
 export class UserShouldSignoutError extends Error {
   constructor() {
     super(
       'The current user must sign out before the new user will be authenticated'
     )
-    this.name = 'UserShouldSignout'
+    this.name = USER_SHOULD_SIGNOUT
   }
 }
