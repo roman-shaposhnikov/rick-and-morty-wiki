bab4c4be6f24ed7ab3115aac3257041ef1bb714c
roman_dector <roman_dector@vk.com>
2023-11-11 16:31:51 +0300

    feat: add navigation menu for User and Guest

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,3 +1,8 @@
+import { Favorite, History } from '@mui/icons-material'
+import { Button } from '@mui/material'
+import { userModel } from 'entities/user'
+import { SignOutButton } from 'features/signout'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import logo from './assets/logo.svg'
@@ -17,10 +22,39 @@ export function Header() {
 }
 
 function HeaderMenu() {
+  const isSignedIn = useSelector(userModel.selectors.isSignedIn)
+
   return (
-    <div>
-      <Link to={'/signin'}>Sign In</Link>
-      <Link to={'/signup'}>Sign Up</Link>
+    <div className={s.menu}>
+      {isSignedIn ? <UserMenu /> : <GuestMenu />}
     </div>
+  )
+}
+
+function UserMenu() {
+  return (
+    <>
+      <Link to={'/favorites'}>
+        <Favorite />
+      </Link>
+      <Link to={'/favorites'}>
+        <History />
+      </Link>
+      <SignOutButton />
+    </>
+  )
+}
+
+function GuestMenu() {
+  return (
+    <>
+      <Link to={'/signin'}>
+        <Button variant='contained'>Sign In</Button>
+      </Link>
+
+      <Link to={'/signup'}>
+        <Button variant='contained'>Sign Up</Button>
+      </Link>
+    </>
   )
 }
