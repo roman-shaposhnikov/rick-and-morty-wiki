ffa86005b889e200e9e296456a6d2f6dc7761c0f
roman_dector <roman_dector@vk.com>
2023-11-11 16:14:02 +0300

    feat: update signin page

--- src/pages/signin/ui/signin.tsx
+++ src/pages/signin/ui/signin.tsx
@@ -1,11 +1,17 @@
 import { SignInForm } from 'features/signin'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export function SignIn() {
+  const navigate = useNavigate()
+
   return (
     <main className='page authPage'>
       <h1>Sign In</h1>
-      <SignInForm />
+      <SignInForm
+        handleSignedIn={() => {
+          navigate('/')
+        }}
+      />
       <Link to={'/signup'}>
         Don't you have an account? Just Sign Up!
       </Link>
