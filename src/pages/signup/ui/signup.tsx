cfa89024170b603bc3034398efd71ae49dc110a5
roman_dector <roman_dector@vk.com>
2023-11-11 16:14:15 +0300

    feat: update signup page

--- src/pages/signup/ui/signup.tsx
+++ src/pages/signup/ui/signup.tsx
@@ -1,3 +1,20 @@
+import { SignUpForm } from 'features/signup'
+import { Link, useNavigate } from 'react-router-dom'
+
 export function SignUp() {
-  return <h1>SignUp page</h1>
+  const navigate = useNavigate()
+
+  return (
+    <main className='page authPage'>
+      <h1>Sign Up</h1>
+      <SignUpForm
+        handleSignedUp={() => {
+          navigate('/')
+        }}
+      />
+      <Link to={'/signin'}>
+        Already have an account? Just Sign In!
+      </Link>
+    </main>
+  )
 }
