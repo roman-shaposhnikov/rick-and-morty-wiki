3ae4850885694fcf2af4df295549e697ccbb621a
roman_dector <roman_dector@vk.com>
2023-11-15 23:39:11 +0300

    ref(features/search): display Loader when request is fetching; display message on empty search results; rename nextPage to setPage

--- src/features/search/ui/page/page.tsx
+++ src/features/search/ui/page/page.tsx
@@ -12,42 +12,61 @@ import s from './style.module.css'
 
 interface Props {
   query: Record<string, string>
-  nextPage: (page: number) => void
+  setPage: (page: number) => void
 }
 
 export function Page(props: Props) {
-  const { data = TEMPLATE_INFO, isLoading } =
-    characterApi.useGetMatchingCharactersQuery(props.query)
+  const {
+    data = TEMPLATE_INFO,
+    isFetching,
+    isError,
+  } = characterApi.useGetMatchingCharactersQuery(props.query)
+
+  let content
+
+  if (isFetching) {
+    content = (
+      <div className='absCentered'>
+        <Loader />
+      </div>
+    )
+  } else if (isError) {
+    content = <h3>Nothing matching was found</h3>
+  } else {
+    content = (
+      <SearchResults
+        info={data}
+        page={+props.query.page}
+        setPage={props.setPage}
+      />
+    )
+  }
 
   return (
     <main className={s.main}>
       <SearchLine query={props.query.name} />
-
-      {isLoading ? (
-        <div className='absCentered'>
-          <Loader />
-        </div>
-      ) : (
-        <SearchResults info={data} nextPage={props.nextPage} />
-      )}
+      {content}
     </main>
   )
 }
 
 function SearchResults({
   info,
-  nextPage,
+  page,
+  setPage,
 }: {
   info: Info<Character[]>
-  nextPage: (page: number) => void
+  page: number
+  setPage: (page: number) => void
 }) {
   return (
     <div className={s.results}>
       <Pagination
         count={info.info.pages}
         shape='rounded'
+        page={page}
         onChange={(_, page) => {
-          nextPage(page)
+          setPage(page)
         }}
       />
       <div className={'cardsLayer'}>
@@ -55,7 +74,14 @@ function SearchResults({
           <CharacterCard key={c.id} info={c} />
         ))}
       </div>
-      <Pagination count={info.info.pages} shape='rounded' />
+      <Pagination
+        count={info.info.pages}
+        shape='rounded'
+        page={page}
+        onChange={(_, page) => {
+          setPage(page)
+        }}
+      />
     </div>
   )
 }
