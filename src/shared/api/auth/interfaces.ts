e25ef0b8968495c7d450565259f3100bb8faa74f
roman_dector <roman_dector@vk.com>
2023-11-17 23:22:26 +0300

    feat(shared/api/auth): update AuthAPI interface

--- src/shared/api/auth/interfaces.ts
+++ src/shared/api/auth/interfaces.ts
@@ -13,9 +13,14 @@ type Authenticate = (creds: Credentials) => Promise<User>
 type SignIn = Authenticate
 type SignUp = Authenticate
 type SignOut = () => Promise<void>
+type shouldSignIn = () => Promise<{
+  result: boolean
+  id: string
+}>
 
 export interface AuthAPI {
   signin: SignIn
   signup: SignUp
   signout: SignOut
+  shouldSignin: shouldSignIn
 }
