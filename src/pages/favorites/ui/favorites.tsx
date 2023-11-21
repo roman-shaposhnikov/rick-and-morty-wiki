18188455c2128b4fd9c4872343c2e06e91252cd5
roman_dector <roman_dector@vk.com>
2023-11-21 12:57:48 +0300

    ref(pages/favorites): add favorites list

--- src/pages/favorites/ui/favorites.tsx
+++ src/pages/favorites/ui/favorites.tsx
@@ -1,3 +1,43 @@
+import { favoritesModel } from 'entities/favorites'
+import { FavoritesList } from 'features/display-favorites'
+import { useLayoutEffect, useMemo } from 'react'
+import { useSelector } from 'react-redux'
+import { useAppDispatch } from 'shared/lib/redux'
+import { Loader } from 'shared/ui'
+
+import s from './style.module.css'
+
 export function Favorites() {
-  return <h1>Favorites page</h1>
+  const dispatch = useAppDispatch()
+
+  const favorites = useSelector(favoritesModel.selectors.favorites)
+  const isLoading = useSelector(favoritesModel.selectors.isLoading)
+
+  const favoritesIds = useMemo(() => {
+    return favorites.map(i => i.id)
+  }, [favorites])
+
+  useLayoutEffect(() => {
+    dispatch(favoritesModel.operations.getFavorites())
+  }, [dispatch])
+
+  if (isLoading) {
+    return (
+      <div className='absCentered'>
+        <Loader />
+      </div>
+    )
+  }
+
+  return (
+    <main className={s.main}>
+      {!favoritesIds.length ? (
+        <div className='absCentered'>
+          <h2>There is nothing in favorites yet</h2>
+        </div>
+      ) : (
+        <FavoritesList favoritesIds={favoritesIds} />
+      )}
+    </main>
+  )
 }
