a627a504806b9d2277fbb6574a71cdf6b997b030
roman_dector <roman_dector@vk.com>
2023-11-21 12:54:57 +0300

    feat(entities/favorites): create favoritesModel

---
+++ src/entities/favorites/model/index.ts
@@ -0,0 +1,7 @@
+import { slice } from './slice'
+
+export const reducer = slice.reducer
+export const actions = slice.actions
+
+export * as operations from './operations'
+export * as selectors from './selectors'
