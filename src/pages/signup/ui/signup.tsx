773f50bfe72504b0c8c1b12520a584370607aad0
roman_dector <roman_dector@vk.com>
2023-11-23 21:23:06 +0300

    feat: add modal Loader on loading in progress

--- src/pages/signup/ui/signup.tsx
+++ src/pages/signup/ui/signup.tsx
@@ -1,20 +1,34 @@
+import { userModel } from 'entities/user'
 import { SignUpForm } from 'features/signup'
+import { useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
+import { Loader, Modal } from 'shared/ui'
 
 export function SignUp() {
   const navigate = useNavigate()
+  const isAuthenticating = useSelector(
+    userModel.selectors.isAuthenticating
+  )
 
   return (
-    <main className='page authPage'>
-      <h1>Sign Up</h1>
-      <SignUpForm
-        handleSignedUp={() => {
-          navigate('/')
-        }}
-      />
-      <Link to={'/signin'}>
-        Already have an account? Just Sign In!
-      </Link>
-    </main>
+    <>
+      <main className='page authPage'>
+        <h1>Sign Up</h1>
+        <SignUpForm
+          handleSignedUp={() => {
+            navigate('/')
+          }}
+        />
+        <Link to={'/signin'}>
+          Already have an account? Just Sign In!
+        </Link>
+      </main>
+
+      {!isAuthenticating ? null : (
+        <Modal>
+          <Loader />
+        </Modal>
+      )}
+    </>
   )
 }
