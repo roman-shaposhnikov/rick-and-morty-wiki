f8b0f5cca61f10fd95bd1faa82a46f5eb1a38f88
roman_dector <roman_dector@vk.com>
2023-11-23 14:13:43 +0300

    ref(shared/api/history): update functions contracts

--- src/shared/api/history/local-storage/history.ts
+++ src/shared/api/history/local-storage/history.ts
@@ -1,14 +1,20 @@
 import { Item } from '../interfaces'
 import { getUserHistory, removeItem, saveItem } from './lib'
 
-export async function save(item: Item, userId: string) {
+export async function save(
+  item: Item,
+  userId: string
+): Promise<Item> {
   return saveItem(item, userId)
 }
 
-export async function remove(timestamp: number, userId: string) {
+export async function remove(
+  timestamp: number,
+  userId: string
+): Promise<void> {
   return removeItem(timestamp, userId)
 }
 
-export async function getAll(userId: string) {
+export async function getAll(userId: string): Promise<Item[]> {
   return getUserHistory(userId)
 }
