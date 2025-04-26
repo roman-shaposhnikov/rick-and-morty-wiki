fbb437f98a045b58b361285dcd5b1402d5d36b8b
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:03 +0300

    feat(entities/favorites): add selectors; add userSingedOut action

--- src/entities/favorites/model/selectors.ts
+++ src/entities/favorites/model/selectors.ts
@@ -1,10 +1,20 @@
 import { createSelector } from '@reduxjs/toolkit'
 
 // sort favorites from new to old
-export const favorites = createSelector(
+const favorites = createSelector(
   (state: RootState) => state.favorites.items,
   items => [...items].reverse()
 )
+
+export const favoritesIds = createSelector(favorites, items =>
+  items.map(i => i.id)
+)
+
+export const isFavorite = (id: number) =>
+  createSelector(
+    (state: RootState) => state.favorites.items,
+    items => !!items.find(i => i.id === id)
+  )
 
 export const isLoading = (state: RootState) =>
   state.favorites.isLoading
