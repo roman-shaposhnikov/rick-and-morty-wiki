b53c1cfebd0eb29ac3f585e8b0dca8922676755f
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:04 +0300

    add link to root page

--- src/shared/ui/fallback-page/page.tsx
+++ src/shared/ui/fallback-page/page.tsx
@@ -1,13 +1,20 @@
 import s from './style.module.css'
 
 export function Page() {
+  const rootPath = encodeURI(
+    `${window.location.origin}/rick-and-morty-wiki/`
+  )
+
   return (
     <main className={s.main}>
       <h3>
-        It looks like an error has occurred, please try to reload or
-        go to the main page
+        It looks like an error has occurred,
+        <br />
+        please try to reload or go to the main page
       </h3>
-      <a className={s.link}>Go to main page</a>
+      <a href={rootPath} className={s.link}>
+        Go to main page
+      </a>
     </main>
   )
 }
