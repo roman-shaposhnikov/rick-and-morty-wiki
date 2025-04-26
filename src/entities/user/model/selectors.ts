52f4d334d182e151c55257577f9646e539d3611f
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:01 +0300

    feat: add error selector

--- src/entities/user/model/selectors.ts
+++ src/entities/user/model/selectors.ts
@@ -2,3 +2,5 @@ export const id = (state: RootState) => state.user.info.id
 export const type = (state: RootState) => state.user.type
 export const isSignedIn = (state: RootState) =>
   state.user.type === 'user'
+
+export const error = (state: RootState) => state.user.error
