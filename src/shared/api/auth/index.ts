92b96a4607f10516cb5e5a4c50e4b8890958d5c4
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:03 +0300

    ref: make dinamic imports

--- src/shared/api/auth/index.ts
+++ src/shared/api/auth/index.ts
@@ -1,5 +1,4 @@
 import { AuthAPI } from './interfaces'
-import { lsAuthAPI } from './local-storage'
 
 export {
   USER_ALREADY_EXISTS,
@@ -8,13 +7,11 @@ export {
 } from './errors'
 export type { Credentials, User } from './interfaces'
 
-// TODO: подумать над await import('./local-storage'), чтобы не импортить все модули
-// попробовать еще раз с default export
-export const authAPI: AuthAPI = (() => {
+export const authAPI: AuthAPI = await (async () => {
   switch (import.meta.env.VITE_REMOTE_STORE) {
     case 'ls':
-      return lsAuthAPI
+      return (await import('./local-storage')).lsAuthAPI
     default:
-      return lsAuthAPI
+      return (await import('./local-storage')).lsAuthAPI
   }
 })()
