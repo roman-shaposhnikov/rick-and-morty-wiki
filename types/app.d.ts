b8f9f895e0aed933a6737f323c6b61384180aa08
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:01 +0300

    ref: update types imports

--- types/app.d.ts
+++ types/app.d.ts
@@ -1,9 +1,9 @@
-type Store = import('app/providers/store.tsx').Store
-type RootState = import('app/providers/store.tsx').RootState
-type AppDispatch = import('app/providers/store.tsx').AppDispatch
+type Store = import('app/providers/store').Store
+type RootState = import('app/providers/store').RootState
+type AppDispatch = import('app/providers/store').AppDispatch
 
-type AppCli = import('app/cli.ts').AppCli
-type Cli = import('app/cli.ts').Cli
+type AppCli = import('app/cli').AppCli
+type Cli = import('app/cli').Cli
 
 interface Window {
   appCli: AppCli
