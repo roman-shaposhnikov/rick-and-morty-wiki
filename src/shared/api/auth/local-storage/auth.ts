a7a32967c1081acb81b42e36bbb43d7176b69f61
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:01 +0300

    ref: clean up signup, signin functions code; update signin, signup behavior

--- src/shared/api/auth/local-storage/auth.ts
+++ src/shared/api/auth/local-storage/auth.ts
@@ -1,55 +1,49 @@
 import { UserNotRegistered, UserShouldSignout } from '../errors'
-import { Credentials, User } from '../interfaces'
-
-const REGISTERED_USERS_LS_KEY = 'registered-users'
-const CURRENT_USER_ID_LS_KEY = 'current-user-id'
+import { Credentials } from '../interfaces'
+import { createUserWithCreds } from '../lib'
+import {
+  addUser,
+  findUserByCreds,
+  setCurrentUserId,
+  shouldSignout,
+} from './lib'
 
 export async function signin(creds: Credentials) {
   if (shouldSignout()) {
     throw new UserShouldSignout()
   }
 
-  const string_users = localStorage.getItem(REGISTERED_USERS_LS_KEY)
-
-  if (string_users === null) {
-    throw new UserNotRegistered()
-  }
-
-  const users: User[] = JSON.parse(string_users)
-
-  const user = users.find(
-    ({ creds: { username, password } }) =>
-      username === creds.username && password === creds.password
-  )
+  const user = findUserByCreds(creds)
 
   if (user === undefined) {
     throw new UserNotRegistered()
   }
 
-  localStorage.setItem(CURRENT_USER_ID_LS_KEY, user.id)
+  setCurrentUserId(user.id)
 
   return user
 }
 
 export async function signup(creds: Credentials) {
-  const string_users = localStorage.getItem(REGISTERED_USERS_LS_KEY)
+  if (shouldSignout()) {
+    throw new UserShouldSignout()
+  }
 
-  const users: User[] =
-    string_users !== null ? JSON.parse(string_users) : []
+  const user = findUserByCreds(creds)
 
-  const newUser: User = { id: Date.now().toString(), creds }
+  if (user !== undefined) {
+    setCurrentUserId(user.id)
+    return user
+  }
 
-  users.push(newUser)
+  const newUser = createUserWithCreds(creds)
 
-  localStorage.setItem(REGISTERED_USERS_LS_KEY, JSON.stringify(users))
+  addUser(newUser)
+  setCurrentUserId(newUser.id)
 
   return newUser
 }
 
-function shouldSignout() {
-  return !!localStorage.getItem(CURRENT_USER_ID_LS_KEY)
-}
-
 export async function signout() {
-  localStorage.setItem(CURRENT_USER_ID_LS_KEY, '')
+  setCurrentUserId('')
 }
