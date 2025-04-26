8510263a8f1d2c95d9579eaa34697942dc23e404
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:03 +0300

    feat: add modal Loader on loading in progress

--- src/pages/root/ui/root.tsx
+++ src/pages/root/ui/root.tsx
@@ -1,20 +1,26 @@
 import { characterApi } from 'entities/character'
 import { SearchLine } from 'features/search'
+import { Loader } from 'shared/ui'
 import { CharacterCard } from 'widgets/character-card'
 
 import s from './style.module.css'
 
 export function Root() {
-  const { data } = characterApi.useGetRandomCharactersQuery(6)
+  const { data, isFetching } =
+    characterApi.useGetRandomCharactersQuery(6)
 
   return (
     <main className={s.main}>
       <SearchLine />
-      <div className={'cardsLayer'}>
-        {data?.map(c => (
-          <CharacterCard key={c.id} {...c} />
-        ))}
-      </div>
+      {isFetching ? (
+        <Loader />
+      ) : (
+        <div className={'cardsLayer'}>
+          {data?.map(c => (
+            <CharacterCard key={c.id} {...c} />
+          ))}
+        </div>
+      )}
     </main>
   )
 }
--- src/pages/signin/ui/signin.tsx
+++ src/pages/signin/ui/signin.tsx
@@ -1,20 +1,34 @@
+import { userModel } from 'entities/user'
 import { SignInForm } from 'features/signin'
+import { useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
+import { Loader, Modal } from 'shared/ui'
 
 export function SignIn() {
   const navigate = useNavigate()
+  const isAuthenticating = useSelector(
+    userModel.selectors.isAuthenticating
+  )
 
   return (
-    <main className='page authPage'>
-      <h1>Sign In</h1>
-      <SignInForm
-        handleSignedIn={() => {
-          navigate('/')
-        }}
-      />
-      <Link to={'/signup'}>
-        Don't you have an account? Just Sign Up!
-      </Link>
-    </main>
+    <>
+      <main className='page authPage'>
+        <h1>Sign In</h1>
+        <SignInForm
+          handleSignedIn={() => {
+            navigate('/')
+          }}
+        />
+        <Link to={'/signup'}>
+          Don't you have an account? Just Sign Up!
+        </Link>
+      </main>
+
+      {!isAuthenticating ? null : (
+        <Modal>
+          <Loader />
+        </Modal>
+      )}
+    </>
   )
 }
