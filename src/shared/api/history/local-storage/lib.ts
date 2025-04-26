c892e3aa0334a04b8a1dfbe2b78b461b4159083d
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:02 +0300

    feat(shared/api/history): add localStorage history api support

---
+++ src/shared/api/history/local-storage/lib.ts
@@ -0,0 +1,40 @@
+import { Item } from '../interfaces'
+
+const HISTORY_KEY = 'history'
+
+type History = Record<string, Item[]>
+
+function getHistory(): History {
+  return JSON.parse(localStorage.getItem(HISTORY_KEY) ?? '{}')
+}
+
+export function getUserHistory(userId: string): Item[] {
+  const history = getHistory()
+
+  return history[userId] ?? []
+}
+
+export function saveItem(item: Item, userId: string) {
+  const history = getHistory()
+  history[userId] ??= []
+
+  history[userId].push(item)
+
+  localStorage.setItem(HISTORY_KEY, JSON.stringify(history))
+
+  return item
+}
+
+export function removeItem(timestamp: number, userId: string) {
+  const history = getHistory()
+
+  if (!history[userId]) {
+    return
+  }
+
+  history[userId] = history[userId].filter(
+    i => i.timestamp !== timestamp
+  )
+
+  localStorage.setItem(HISTORY_KEY, JSON.stringify(history))
+}
