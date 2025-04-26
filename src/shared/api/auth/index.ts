d34f9fddace58b8150d81776eb434a0657d18c7d
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:03 +0300

    ref(shared/api/auth): remove unexists export; rename lsAuthAPI to lsAuth

--- src/shared/api/auth/index.ts
+++ src/shared/api/auth/index.ts
@@ -1,17 +1,13 @@
 import { AuthAPI } from './interfaces'
 
-export {
-  USER_ALREADY_EXISTS,
-  USER_NOT_REGISTERED,
-  USER_SHOULD_SIGNOUT,
-} from './errors'
+export { USER_NOT_REGISTERED, USER_SHOULD_SIGNOUT } from './errors'
 export type { Credentials, User } from './interfaces'
 
 export const authAPI: AuthAPI = await (async () => {
   switch (import.meta.env.VITE_REMOTE_STORE) {
     case 'ls':
-      return (await import('./local-storage')).lsAuthAPI
+      return (await import('./local-storage')).lsAuth
     default:
-      return (await import('./local-storage')).lsAuthAPI
+      return (await import('./local-storage')).lsAuth
   }
 })()
