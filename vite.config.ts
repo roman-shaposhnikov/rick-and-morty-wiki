4ea989513f3133ea7b9e722077976047ae9a50d7
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:00 +0300

    feat: set up env vars VITE_REMOTE_STORE, BASE_PUBLIC_PATH; create authAPI

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  base: '/rick-and-morty-wiki/',
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    base: env.BASE_PUBLIC_PATH,
+  })
+}
