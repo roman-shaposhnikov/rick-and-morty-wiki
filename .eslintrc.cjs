d26de47ba8385b11448ff57782e8f6d740d35c7a
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:01 +0300

    ref: update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,7 @@ module.exports = {
     'import',
   ],
   rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
     'no-console': 'warn',
     curly: 'error',
