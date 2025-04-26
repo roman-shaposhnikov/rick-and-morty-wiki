1911b5c270d697833f6ad5258f2ba5e234a00f4b
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:29:59 +0300

    feat: create redux store

---
+++ src/app/App.tsx
@@ -0,0 +1,7 @@
+import { withProviders } from './providers'
+
+function App() {
+  return <></>
+}
+
+export default withProviders(App)
---
+++ types/app.d.ts
@@ -0,0 +1,6 @@
+type RootState = import('app/providers/store.tsx').RootState
+type AppDispatch = import('app/providers/store.tsx').AppDispatch
+
+interface Window {
+  __store__: unknown
+}
