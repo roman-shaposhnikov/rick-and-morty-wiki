47fcf1bb51a5869d8a1fe8bbc6d17cc5ab21abec
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:03 +0300

    feat: add favorites middleware

--- src/app/providers/store/middlewares/index.ts
+++ src/app/providers/store/middlewares/index.ts
@@ -1,10 +1,12 @@
 import { characterApi } from 'entities/character'
 import { episodeApi } from 'entities/episode'
 
+import { favoritesMiddleware } from './favorites'
 import { historyMiddleware } from './history'
 
 export const middlewares = [
-  historyMiddleware,
+  historyMiddleware.middleware,
+  favoritesMiddleware.middleware,
 
   characterApi.middleware,
   episodeApi.middleware,
