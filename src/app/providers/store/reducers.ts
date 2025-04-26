552d7018ea7ba9b77c59db1408c70598e74c1146
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:03 +0300

    feat(app/store): add favoritesModel reducer

--- src/app/providers/store/reducers.ts
+++ src/app/providers/store/reducers.ts
@@ -1,12 +1,14 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import { characterApi } from 'entities/character'
 import { episodeApi } from 'entities/episode'
+import { favoritesModel } from 'entities/favorites'
 import { historyModel } from 'entities/history'
 import { userModel } from 'entities/user'
 
 export const rootReducer = combineReducers({
   user: userModel.reducer,
   history: historyModel.reducer,
+  favorites: favoritesModel.reducer,
   [characterApi.reducerPath]: characterApi.reducer,
   [episodeApi.reducerPath]: episodeApi.reducer,
 })
