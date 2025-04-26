be19ace772eaa1d5b7474d8cc7d87680dda47677
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:02 +0300

    feat(app): add initialization

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,18 @@
 import './styles/global.css'
 
+import { Loader } from 'shared/ui'
+
+import { useInit } from './hooks'
 import { Router } from './router'
 
 export function App() {
-  return <Router />
+  const isInitialized = useInit()
+
+  const loader = (
+    <div className='absCentered'>
+      <Loader />
+    </div>
+  )
+
+  return !isInitialized ? loader : <Router />
 }
