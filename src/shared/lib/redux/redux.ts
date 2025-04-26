1f4528d58c8d20bd34c89ffd048d7038a495e44a
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:00 +0300

    ref: change params order in createAppThunk

--- src/shared/lib/redux/redux.ts
+++ src/shared/lib/redux/redux.ts
@@ -7,7 +7,7 @@ import { useDispatch } from 'react-redux'
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 
-export const createAppThunk = <Returned = void, ThunkArg = void>(
+export const createAppThunk = <ThunkArg = void, Returned = void>(
   typePrefix: string,
   payloadCreator: AsyncThunkPayloadCreator<
     Returned,
