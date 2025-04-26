0163780eeb3ed661da1a4490ac0ba2333f5061d0
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:02 +0300

    feat(app/store): add historyModel.reducer

--- src/app/providers/store/reducers.ts
+++ src/app/providers/store/reducers.ts
@@ -1,9 +1,12 @@
+import { combineReducers } from '@reduxjs/toolkit'
 import { characterApi } from 'entities/character'
 import { episodeApi } from 'entities/episode'
+import { historyModel } from 'entities/history'
 import { userModel } from 'entities/user'
 
-export const reducers = {
+export const rootReducer = combineReducers({
   user: userModel.reducer,
+  history: historyModel.reducer,
   [characterApi.reducerPath]: characterApi.reducer,
   [episodeApi.reducerPath]: episodeApi.reducer,
-}
+})
