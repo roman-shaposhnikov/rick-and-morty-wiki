a4a1dc2c70237060e41d42ddf86594827a6109bf
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:03 +0300

    feat: make FavoriteButton visible only for signed in user

--- src/widgets/character-card/ui/card.tsx
+++ src/widgets/character-card/ui/card.tsx
@@ -1,11 +1,15 @@
 import { CharacterCard } from 'entities/character'
+import { userModel } from 'entities/user'
 import { FavoriteButton } from 'features/add-to-favorite'
+import { useSelector } from 'react-redux'
 import { Character } from 'shared/api/data'
 
 export function Card(props: Character) {
+  const isSignedIn = useSelector(userModel.selectors.isSignedIn)
+
   return (
     <CharacterCard info={props}>
-      <FavoriteButton id={props.id} />
+      {isSignedIn ? <FavoriteButton id={props.id} /> : null}
     </CharacterCard>
   )
 }
