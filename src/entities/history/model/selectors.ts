92eba3f125e3052c6bb7de1958a910ad575d9b02
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:02 +0300

    ref(entities/history): sort history from new to old

--- src/entities/history/model/selectors.ts
+++ src/entities/history/model/selectors.ts
@@ -1,2 +1,9 @@
-export const history = (state: RootState) => state.history.items
+import { createSelector } from '@reduxjs/toolkit'
+
+// sort search history from new to old
+export const history = createSelector(
+  (state: RootState) => state.history.items,
+  items => [...items].reverse()
+)
+
 export const isLoading = (state: RootState) => state.history.isLoading
