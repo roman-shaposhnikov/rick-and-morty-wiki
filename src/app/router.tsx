f99f1f4513079d45b6dc4d895d509d0f1ef94ca5
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:02 +0300

    feat(app/router): add lazy loading for history page

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,31 +1,42 @@
 import { Character } from 'pages/character'
 import { Favorites } from 'pages/favorites'
-import { History } from 'pages/history'
 import { Root } from 'pages/root'
 import { Search } from 'pages/search'
 import { SignIn } from 'pages/signin'
 import { SignUp } from 'pages/signup'
+import { lazy, Suspense } from 'react'
 import { HashRouter, Route, Routes } from 'react-router-dom'
+import { Loader } from 'shared/ui'
 import { Header } from 'widgets/header'
+
+const History = lazy(() => import('pages/history'))
 
 export function Router() {
   return (
     <HashRouter>
       <Header />
 
-      <Routes>
-        <Route path='/' element={<Root />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/favorites' element={<Favorites />} />
-        <Route path='/history' element={<History />} />
-        <Route path='/search' element={<Search />} />
+      <Suspense
+        fallback={
+          <div className='absCentered'>
+            <Loader />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path='/' element={<Root />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/favorites' element={<Favorites />} />
+          <Route path='/history' element={<History />} />
+          <Route path='/search' element={<Search />} />
 
-        <Route path='/character' element={<Character />}>
-          <Route index element={<Character />} />
-          <Route path=':id' element={<Character />} />
-        </Route>
-      </Routes>
+          <Route path='/character' element={<Character />}>
+            <Route index element={<Character />} />
+            <Route path=':id' element={<Character />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </HashRouter>
   )
 }
