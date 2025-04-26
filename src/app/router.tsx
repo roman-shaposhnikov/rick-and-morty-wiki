9df6cecfa0ba65f292b70d00f3784416244c8fb1
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:30:00 +0300

    chore: add base to vite config

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,9 +1,14 @@
 import { Root } from 'pages/root'
+import { SignUp } from 'pages/signup'
 import { createHashRouter } from 'react-router-dom'
 
 export const router = createHashRouter([
   {
     path: '/',
     element: <Root />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
   },
 ])
